8451de25d21f3d36fe81ad77dba9861de6f9ab96
RahulPraj <rahul971801@gmail.com>
2026-02-12 14:58:37 +0530

    second commit

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,10 +1,67 @@
 import React from "react";
-
+import Navbar from "../components/Navbar";
 
 function Todo(){
     return(
-        <div>
-            i am todo
+        <div className="todo-page">
+            <Navbar/>
+            <div className="todo-wrap">
+                <div className="todo-header">
+                    <h2 className="todo-title">Todo Dashboard</h2>
+                    <span className="todo-badge">Pending: {pending}</span>
+                </div>
+            </div>
+
+            <div className="todo-add-row">
+                <input
+                 className="todo-input"
+                 type="text"
+                  name=""
+                  value={}
+                  onChange={}
+                  placeholder="Add new Task"  
+                 />
+                <button type="submit"
+                 className="todo-btn todo-btn-add" 
+                onClick={}
+                >
+                    Add
+                </button>
+            </div>
+            <input 
+                className="todo-input todo-search"
+                type="text" 
+                name="" 
+                placeholder="Search Tasks...."
+                value={}
+                onChange={}
+             />
+
+            <div className="todo-list">
+                {
+                    filtered.map(t=>(
+                        <div className={t.done ? "todo-row todo-row-done" :"todo-row"} 
+                        key={t.id}>
+                         <span>{t.text}</span>  
+                         <button 
+                          className="todo-btn todo-btn-done"
+                          type="button"
+                          onClick={}
+                          >
+                            Done
+                        </button> 
+                        <button
+                          type="button"
+                          className="todo-btn todo-btn-del"
+                          onClick={} 
+                        >
+                            Delete
+                        </button>    
+                        </div>
+                    ))
+                }
+
+            </div>
         </div>
     )
 }
